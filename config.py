cc1054945a8b671396ab339aa5f77f078aac0d09
Cory Johns <cory@nextowntech.com>
2012-03-19 13:38:42 -0400

    [#5] Refactored config and dispatch, added help

---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+import imp
+
+
+class Config(object):
+    BRANCH_PATTERN = r'^\w{2,3}/(\d+)'
+    COMMIT_PATTERN = '[#{story_num}] {message}'
+
+    def __init__(self):
+        self.load_rcfile(self.find_rcfile())
+
+    def find_rcfile(self):
+        with os.popen('git rev-parse --show-toplevel') as p:
+            gitdir = p.readline().rstrip('\n') + '/.git'
+        usrdir = os.path.expanduser('~')
+        for rcfile in [gitdir+'/.dhrc', usrdir+'/.dhrc', None]:
+            if rcfile is None or os.path.exists(rcfile):
+                break
+        return rcfile
+
+    def load_rcfile(self, rcfile):
+        if rcfile is None:
+            return
+        rc = imp.load_source('devhelpers_rc', rcfile)
+        for p in dir(rc):
+            if not p.startswith('__'):
+                setattr(self, p, getattr(rc, p))
--- helpers.py
+++ helpers.py
@@ -2,16 +2,35 @@
 
 import sys
 import re
-import os
-import imp
 
-COMMANDS = ['story_from_branch', 'format_commit_message', 'git_opts']
+from config import Config
 
-config = None  # loaded below
+COMMANDS = ['help', 'story_from_branch', 'format_commit_message', 'git_opts']
 
+config = Config()
+
+
+def _argnames(func):
+    if callable(func):
+        return ' '.join([n.upper() for n in func.func_code.co_varnames[:func.func_code.co_argcount]])
+    else:
+        return ''
+
+
+def help():
+    '''Displays this help message.'''
+    print 'Usage: %s COMMAND OPTIONS'
+    print
+    print 'The available COMMANDS are:'
+    for command in COMMANDS:
+        func = globals().get(command, None)
+        if callable(func):
+            print '    %-40s    %s' % (command + ' ' + _argnames(func), func.__doc__)
+        else:
+            print "    %-40s    Returns the value for %s (currently '%s')" % (command, command.upper(), resolve(command))
 
 def story_from_branch(branch_name):
-    """Usage: %s story_from_branch BRANCH_NAME"""
+    '''Extracts the story number from a branch name, based on the conventions defined in the .dhrc file.'''
     story_number = re.search(config.BRANCH_PATTERN, branch_name)
     if story_number is not None:
         print story_number.group(1)
@@ -19,55 +38,36 @@ def story_from_branch(branch_name):
         print >> sys.stderr, 'Fatal: Could not get story number from branch'
 
 def format_commit_message(story_num, message):
-    """Usage: %s format_commit_message STORY_NUM MESSAGE"""
+    '''Formats a commit message, given a story number, based on the conventions defined in the .dhrc file.'''
     print config.COMMIT_PATTERN.format(
         story_num=story_num,
         message=message,
     )
 
-def git_opts():
-    print config.GIT_OPTS
+def resolve(prop):
+    '''Resolves a basic property and sends it back to the user.'''
+    return getattr(config, prop.upper(), '')
 
-
-class Config(object):
-    BRANCH_PATTERN = r'^\w{2,3}/(\d+)'
-    COMMIT_PATTERN = '[#{story_num}] {message}'
-    GIT_OPTS = ''
-
-    def __init__(self):
-        self.load_rcfile(self.find_rcfile())
-
-    def find_rcfile(self):
-        with os.popen('git rev-parse --show-toplevel') as p:
-            gitdir = p.readline().rstrip('\n') + '/.git'
-        usrdir = os.path.expanduser('~')
-        for rcfile in [gitdir+'/.dhrc', usrdir+'/.dhrc', None]:
-            if rcfile is None or os.path.exists(rcfile):
-                break
-        return rcfile
-
-    def load_rcfile(self, rcfile):
-        if rcfile is None:
-            return
-        rc = imp.load_source('devhelpers_rc', rcfile)
-        for p in dir(rc):
-            if not p.startswith('__'):
-                setattr(self, p, getattr(rc, p))
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
-        print >> sys.stderr, 'Usage: %s COMMAND OPTIONS ...'
+        help()
         sys.exit(1)
-
-    config = Config()
 
     prop = sys.argv[1]
+    args = sys.argv[2:]
+    if prop not in COMMANDS:
+        print >> sys.stderr, 'Fatal: Unknown command: '+prop
+        help()
+        sys.exit(1)
+
     func = globals().get(prop, None)
-    if prop not in COMMANDS or not callable(func):
-        print >> sys.stderr, 'ERROR: Unknown command: '+prop
+    argcount = func.func_code.co_argcount if callable(func) else 0
+    if len(args) != argcount:
+        print >> sys.stderr, 'Usage: %s %s %s' % (sys.argv[0], prop, _argnames(func))
         sys.exit(1)
-    try:
-        func(*sys.argv[2:])
-    except TypeError:
-        print >> sys.stderr, func.__doc__ % sys.argv[0]
-        sys.exit(1)
+
+    if callable(func):
+        func(*args)
+    else:
+        print resolve(prop)
