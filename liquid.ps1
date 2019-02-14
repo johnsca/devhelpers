7e4356fcb550f666d63bb493f008d4c1030a6355
Cory Johns <johnsca@gmail.com>
2019-02-14 11:54:42 -0500

    Re-enable and tweak VC info

--- liquid.ps1
+++ liquid.ps1
@@ -36,14 +36,16 @@ then
     LP_PS1="${LP_PS1}${LP_PWD}]${LP_VENV}${LP_JENV}${LP_PROXY}"
     # add VCS infos
     LP_PS1="${LP_PS1}${LP_VCS}"
+    LP_MARK='$'
 else
     # path in yellow
     LP_PS1="${LP_PS1}${LP_PWD}]${LP_VENV}${LP_JENV}${LP_PROXY}"
     # do not add VCS infos unless told otherwise (LP_ENABLE_VCS_ROOT)
     [[ "$LP_ENABLE_VCS_ROOT" = "1" ]] && LP_PS1="${LP_PS1}${LP_VCS}"
+    LP_MARK='#'
 fi
 # add return code and prompt mark
-LP_PS1="${LP_PS1}${LP_RUNTIME}${LP_ERR} ${LP_MARK}"
+LP_PS1="${LP_PS1}${LP_RUNTIME}${LP_ERR} ${LP_MARK} "
 
 # "invisible" parts
 # Get the current prompt on the fly and make it a title
